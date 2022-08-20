a5ac5b1e7b479426258a8b69379275f25796db1e
HeroxeT <schigorin@mail.ru>
2022-08-20 19:34:30 +0300

    feat: change render function

--- src/Client.tsx
+++ src/Client.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 import { App } from 'App'
 
-const entryBlock = document.getElementById('root')
-const renderFunction: ReactDOM.Renderer = entryBlock.hasChildNodes() ? ReactDOM.hydrate : ReactDOM.render
-
-renderFunction(<BrowserRouter>
-    <App />
-</BrowserRouter>, entryBlock)
+const root = createRoot(document.getElementById('root')!)
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+)
